dc78990c6449947fed21d19c836fd36cc073b693
Swapnil Sarvankar <swapnil19111987@gmail.com>
2015-12-03 19:27:01 +0530

    Issue #2628164: Check for library during both installation and while using the library

--- js/responsive_blocks.js
+++ js/responsive_blocks.js
@@ -10,6 +10,7 @@
              *  Setup Relationship between blocks. Create array to work with mediaCheck library
              */
             var responsive_blocks = convert(settings.responsive_blocks.responsive_blocks);
+			//console.log(responsive_blocks);
             var device_width = convert(settings.responsive_blocks.device_width);
             var desktop = device_width['responsive_blocks_desktop'] + 'px';
             var ipad = device_width['responsive_blocks_ipad'] + 'px';
@@ -18,6 +19,10 @@
             var desktop_blocks = [];
             var ipad_blocks = [];
             var mobile_blocks = [];
+			var desktop_min = parseInt(device_width['responsive_blocks_ipad']) + 1;
+			var ipad_min = parseInt(device_width['responsive_blocks_mobile']) + 1;;
+			desktop_min = desktop_min + 'px';
+			ipad_min = ipad_min + 'px';
             for (var key in responsive_blocks) {
                 block_name = key.split("-");
                 var block = block_name[block_name.length - 1];
@@ -38,7 +43,7 @@
              *  mediaCheck : For Desktop resolution
              */
             mediaCheck({
-                media: '(max-width: ' + desktop + ')',
+                media: '(min-width: ' + desktop_min + ') and (max-width: ' + desktop + ')',
                 entry: function() {
                     for (var key in desktop_blocks) {
                         $('#' + desktop_blocks[key]).show();
@@ -55,7 +60,7 @@
              *  mediaCheck : For IPAD resolution
              */
             mediaCheck({
-                media: '(max-width: ' + ipad + ')',
+                media: '(min-width: ' + ipad_min + ') and (max-width: ' + ipad + ')',
                 entry: function() {
                     for (var key in ipad_blocks) {
                         $('#' + ipad_blocks[key]).show();
@@ -79,7 +84,7 @@
                     }
                 },
                 exit: function() {
-                    for (var key in ipad_blocks) {
+                    for (var key in mobile_blocks) {
                         $('#' + mobile_blocks[key]).hide();
                     }
                 }
